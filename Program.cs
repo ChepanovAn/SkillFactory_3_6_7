e8fe191fc133f0dd5c6bfba23141f069a485e14d
Анатолий <Chepanov199287@gmail.com>
2023-03-21 13:41:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_3_6_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 10 % 3;
+            Console.WriteLine(result);
+        }
+    }
+}
